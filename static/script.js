dacb49667c16d60d04b3095caa59a36e7124bcdc
Keon Lee <keon.lee0404@gmail.com>
2024-06-17 02:35:56 -0400

    Fixed Contact Form

--- static/script.js
+++ static/script.js
@@ -1,2 +1,53 @@
+window.onload = function() {
+    document.getElementById('contact-form').addEventListener('submit', function(event) {
+        const name = event.target.name.value
+        const email = event.target.email.value
+        const message = event.target.message.value
 
+        console.log(name, email, message, experience)
+
+        const errorElement = document.querySelector('#error')
+
+        let messages = []
+
+        if (name === '' || name == null) {
+            messages.push('Name is required')
+        }
+
+        // This will check that the Email input isnt empty, and contains an @ and .
+        if (email === '' || email == null) {
+            messages.push('Email is required')
+        }
+
+        if (email.includes('@') === false || email.includes('.') === false) {
+            messages.push('Email is missing "." or "@"')
+        }
+
+        // This will check that the Message input isnt empty
+        if (message === '' || message == null) {
+            messages.push('Message is required')
+        }
+
+        // This will check that at leaast one radio input is checked
+        if (experience === '') {
+            messages.push('Experience rating required')
+        }
+
+        // This will push the error strings to the div with id of error
+        if (messages.length > 0) {
+            event.preventDefault()
+            errorElement.style.display = 'inline'
+            errorElement.innerText = messages.join(', ')
+        }
+        else {
+            event.preventDefault();
+            emailjs.sendForm('service_czldkd4', 'template_ln7ucnw', this)
+                .then(() => {
+                    console.log('SUCCESS!');
+                }, (error) => {
+                    console.log('FAILED...', error);
+                });
+        }
+    })
+}
 
